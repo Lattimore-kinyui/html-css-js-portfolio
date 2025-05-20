03032c11ff8eb8fbd7e8f4bbec0ca3e4bf30c50f
Lattimore-kinyui <lattimorekinyui@gmail.com>
2025-05-19 21:58:41 -0800

    profile picture

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ function toggleMenu() {
   const icon = document.querySelector('.hamburger-icon');
   menu.classList.toggle('open');
   icon.classList.toggle('open');
-    
 }
+
+// Add this code to attach the event listener
+document.querySelector('.hamburger-icon').addEventListener('click', toggleMenu);
